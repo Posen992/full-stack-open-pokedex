0395579b03bc3af9ed9620dd5e24dd268c76bf81
Posen992 <59381541+Posen992@users.noreply.github.com>
2024-07-26 23:59:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  if (true) throw('error...  ')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
   res.send('ok')
 })
 
